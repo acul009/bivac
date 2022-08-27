638566e6c7ebed2525f029e7dc3a0700d0470d4a
acul <acul009@gmail.com>
2022-08-27 02:51:41 +0200

    prepared restore dialog

---
+++ bivac-ui/src/components/SnapshotList.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import bivac from '../bivac'
+import Table from './Table.vue'
+import Row from './Row.vue'
+import Cell from './Cell.vue'
+import { computed, onMounted, onUnmounted, reactive, ref } from 'vue';
+
+const props = defineProps<{
+  id: string
+}>()
+
+const emit = defineEmits<{
+  (e: "selected", id: string | undefined): void;
+}>();
+
+</script>
+
+<template>
+</template>
+
+<style scoped>
+
+</style>
--- bivac-ui/src/components/VolumeRestore.vue
+++ bivac-ui/src/components/VolumeRestore.vue
@@ -3,20 +3,32 @@ import bivac from '../bivac'
 import Table from './Table.vue'
 import Row from './Row.vue'
 import Cell from './Cell.vue'
-import { computed, onMounted, onUnmounted, reactive, ref } from 'vue';
+import { computed, onMounted, onUnmounted, reactive, ref, type Ref } from 'vue';
+import SnapshotList from './SnapshotList.vue';
 
 const props = defineProps<{
   id: string
 }>()
 
-const vol = computed(() => {return bivac.volumes.value[props.id]})
+const vol = computed(() => { return bivac.volumes.value[props.id] })
+
+const selectedSnapshot: Ref<undefined | string> = ref(undefined)
 
 
 </script>
 
 <template>
+  <div class="wrapper">
+    <SnapshotList :id="id" @selected="(id) => { selectedSnapshot = id }"></SnapshotList>
+  </div>
 </template>
 
 <style scoped>
+
+.wrapper {
+    width: 100%;
+    padding: 20px;
+    border: 1px solid var(--color-border);
+}
 
 </style>
